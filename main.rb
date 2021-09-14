65bc7f449112cd425e5e218f09a869a2f01f6cc4
Sergey <koleshkoser@gmail.com>
2021-09-14 13:57:03 +0300

    refactoring

--- main.rb
+++ main.rb
@@ -1,25 +1,22 @@
-def print_result(top)
-  stop = 0
-  top.each do |name, count|
-    if stop == 5
-      break
-    else
-      puts "#{name} \t #{count}"
-      stop += 1
-    end
+MAX_TOP = 5
+
+def print_result(sorted_files)
+  sorted_files.take(MAX_TOP).each do |name, count|
+    puts "#{name} \t #{count}"
   end
 end
 
-def get_top(data)
-  expansion = data.map { |item| File.extname(item) }.reject(&:empty?)
-  top = expansion.uniq.to_h { |x| [x.to_sym, expansion.count(x)] }.sort_by { |key, value| -value }
+def sorted(files)
+  extensions = files.map { |file| File.extname(file) }.reject(&:empty?)
+  extensions
+    .reduce(Hash.new(0)) do |result, ext|
+      result[ext] += 1
+      result
+    end
+    .sort_by { |_, count| count }.reverse
 end
 
-
-
 path = "test_data/test-1"
-Dir.chdir(path)
-data = Dir.glob("**/*")
+files = Dir.glob("#{path}/**/*")
 
-
-print_result(get_top(data))
+print_result(sorted(files))
